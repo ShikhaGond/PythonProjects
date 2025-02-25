15cf23381496f4040c7288f5b6ae9f068e7a9455
Shikha Gond <87279696+ShikhaGond@users.noreply.github.com>
2025-02-25 16:28:13 +0530

    ipAddress.py

---
+++ ipAddress.py
@@ -0,0 +1,24 @@
+import socket
+import requests
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        local_ip = s.getsockname()[0]
+    except Exception as e:
+        local_ip = "Unable to determine local IP"
+    finally:
+        s.close()
+    return local_ip
+
+def get_public_ip():
+    try:
+        public_ip = requests.get("https://api.ipify.org").text
+    except Exception as e:
+        public_ip = "Unable to determine public IP"
+    return public_ip
+
+if __name__ == "__main__":
+    print("Local IP Address:", get_local_ip())
+    print("Public IP Address:", get_public_ip())
